c8e03f32040f757903a59b1d2fb27074f88d728e
Mikael Bonnier <mikael.bonnier@gmail.com>
2021-04-10 01:20:15 +0200

    Added php.

---
+++ aritm.php
@@ -0,0 +1,430 @@
+#!/usr/bin/env php
+<?php
+# aritm v0.3.3 for php is foss.
+# (c) 1992-2021 by mikael o. bonnier, lund, sweden.
+# e-mail: <mikael.bonnier(a)gmail.com>.
+# absolutely no warranty.
+# license gplv3+, see
+# <http://www.gnu.org/licenses/gpl.html>.
+# more programs at
+# <http://www.df.lth.se.orbin.se/*mikaelb/basic/>. * is tilde.
+# documentation:
+# 76543210, 7:type, 6-4:op1, 3-2:op2, 1-0:/todo.
+# program:
+goto start;
+
+function mod() {
+global $md, $x, $y;
+$md=floor(0.5+($x/$y-floor($x/$y))*$y);
+return;
+}
+
+function div() {
+global $dv, $x, $y;
+$dv=floor(0.5+$x/$y);
+return;
+}
+
+start:
+srand();
+function rnd() {
+return mt_rand()/(mt_getrandmax()+1.0);
+}
+$r=rnd();
+$l=0;
+$m=0;
+cls();
+$aa=array_fill(1, 590, 0.0);
+
+c:
+cls();
+$a=0;
+if (($l>0) and $m) goto contmenu;
+if (0==$m) $m=10;
+goto menu;
+
+s:
+cls();
+print "generating\n";
+print "problems...\n";
+$u=1;
+# $aa=array_fill(1, n, 0.0);
+add1:
+$x=$m;
+$y=10;
+div();
+$x=$dv;
+mod();
+if (0==$md) goto add2;
+for ($i=0; $i<=9; ++$i) {
+for ($j=0; $j<=9; ++$j) {
+$aa[$u]=10000000+$i*10000+$j*100+1;
+$u=$u+1;
+} # $j
+} # $i
+
+add2:
+$x=$m;
+$y=100;
+div();
+$x=$dv;
+$y=10;
+mod();
+if (0==$md) goto sub1;
+for ($i=0; $i<=9; ++$i) {
+for ($j=0; $j<=9; ++$j) {
+$t=10*(floor(8*rnd())+1);
+$aa[$u]=20000000+($t+$i)*10000+$j*100+1;
+$u=$u+1;
+} # $j
+} # $i
+
+sub1:
+$x=$m;
+$y=1000;
+div();
+$x=$dv;
+$y=10;
+mod();
+if (0==$md) goto sub2;
+for ($i=0; $i<=9; ++$i) {
+for ($j=$i; $j<=9+$i; ++$j) {
+$aa[$u]=30000000+$j*10000+$i*100+1;
+$u=$u+1;
+} # $j
+} # $i
+
+sub2:
+$x=$m;
+$y=10000;
+div();
+$x=$dv;
+$y=10;
+mod();
+if (0==$md) goto mul;
+for ($i=0; $i<=9; ++$i) {
+for ($j=$i; $j<=9+$i; ++$j) {
+$t=10*(floor(9*rnd())+1);
+$aa[$u]=40000000+($t+$j)*10000+$i*100+1;
+$u=$u+1;
+} # $j
+} # $i
+
+mul:
+$x=$m;
+$y=100000;
+div();
+$x=$dv;
+$y=10;
+mod();
+if (0==$md) goto divi;
+for ($i=0; $i<=9; ++$i) {
+for ($j=0; $j<=9; ++$j) {
+$aa[$u]=50000000+$i*10000+$j*100+1;
+$u=$u+1;
+} # $j
+} # $i
+
+divi:
+$x=$m;
+$y=1000000;
+div();
+$x=$dv;
+$y=10;
+mod();
+if (0==$md) goto endif3;
+for ($i=0; $i<=9; ++$i) {
+for ($j=1; $j<=9; ++$j) {
+$t=$i*$j+floor($j*rnd());
+$aa[$u]=60000000+$t*10000+$j*100+1;
+$u=$u+1;
+} # $j
+} # $i
+endif3:
+$u=$u-1;
+$l=$u;
+
+u:
+print "shuffling...\n";
+for ($i=$u; $i>=2; --$i) {
+$j=floor($i*rnd())+1;
+$t=$aa[$i];
+$aa[$i]=$aa[$j];
+$aa[$j]=$t;
+} # $i
+$k=1;
+k:
+cls();
+print $l;
+print " problems left. -1 esc\n";
+$t=$aa[$k];
+$x=$t;
+$y=10000000;
+div();
+$c=$dv;
+if (6==$c) print "integer part of ";
+$x=$t;
+$y=10000;
+div();
+$x=$dv;
+$y=1000;
+mod();
+$i=$md;
+print $i;
+sign();
+$x=$t;
+$y=100;
+div();
+$x=$dv;
+$y=100;
+mod();
+$j=$md;
+print $j;
+print " = ";
+$ans = readline();
+$a=(float)$ans;
+# print $ans;
+if (-1==$a or ".1"==$ans) goto c;
+if ((1==$c)+(2==$c)) { $r=$i+$j; goto endif4; }
+if ((3==$c)+(4==$c)) { $r=$i-$j; goto endif4; }
+if (5==$c) { $r=$i*$j; goto endif4; }
+if (6==$c) $r=floor($i/$j);
+endif4:
+if ($r!=$a) goto wrong;
+print "right.\n";
+$x=$t;
+$y=100;
+mod();
+if (0>=$md) goto endif5;
+$l=$l-1;
+$aa[$k]=$t-1;
+endif5:
+$k=$k+1;
+if (!(0<$l)) goto endif6;
+$d=500;
+delay();
+endif6:
+goto endif7;
+wrong:
+print "wrong. ";
+print $i;
+sign();
+print $j;
+print " is ";
+print $r;
+print ".\n";
+$x=$t;
+$y=100;
+mod();
+if (99<=$md) goto endif8;
+$l=$l+1;
+$aa[$k]=$t+1;
+endif8:
+$k=$k+1;
+sound();
+$d=1000;
+delay();
+endif7:
+if ($k<=$u) goto k;
+if (0>=$l) goto w;
+print "checking...\n";
+$n=1;
+for ($k=1; $k<=u; ++$k) {
+$t=$aa[$k];
+$x=$t;
+$y=100;
+mod();
+if (0==$md) goto endif9;
+$aa[$n]=$t;
+$n=$n+1;
+endif9:
+} # $k
+$u=$n-1;
+goto u;
+w:
+print "good.   well done.\n";
+$d=5000;
+delay();
+goto c;
+e:
+cls();
+$a=0;
+$n=0;
+$t=$m;
+sub();
+print "addition 1\n";
+sub();
+print "addition 2\n";
+sub();
+print "subtraction 1\n";
+sub();
+print "subtraction 2\n";
+sub();
+print "multiplication\n";
+sub();
+print "division | -1 esc\n";
+print "0 ok and go ";
+print "$n\n";
+goto input;
+
+function sub() {
+global $a, $x, $t, $y, $dv, $md, $n;
+$a=$a+1;
+$x=$t;
+$y=10;
+div();
+$t=$dv;
+print $a;
+$x=$t;
+$y=10;
+mod();
+if (0==$md) goto else2;
+print "*";
+$n=$n+100-10*(6==$a);
+goto endif10;
+else2:
+print " ";
+endif10:
+return;
+}
+
+input:
+print "toggle item 1-6, or choose 0\n";
+print "or -1: ";
+$ans = readline();
+$a=(float)$ans;
+# print $ans;
+if ((-1>$a)+(6<$a)+(0==$a)*(0==$n)) goto input;
+if (0==$a) goto s;
+if (-1==$a or ".1"==$ans) goto c;
+pow10();
+$x=$m;
+$y=$r;
+div();
+$x=$dv;
+$y=10;
+mod();
+if (0==$md) goto else3;
+$m=$m-$r;
+goto endif11;
+else3:
+$m=$m+$r;
+endif11:
+goto e;
+
+h:
+help();
+goto c;
+
+a:
+about();
+goto c;
+
+q:
+cls();
+exit(0);
+
+contmenu:
+menuitem();
+print "continue\n";
+
+menu:
+menuitem();
+print "setup and go\n";
+menuitem();
+print "help\n";
+menuitem();
+print "about\n";
+menuitem();
+print "exit\n";
+input2:
+print "choose 1-";
+print 4+($l>0);
+print ": ";
+$ans = readline();
+$s=(int)$ans;
+# print $ans;
+if ((1>$s) or (4+($l>0)<$s)) goto input2;
+$s=$s+($l<=0);
+# on s goto k,e,h,a,q
+switch($s) {
+case 1: goto k;
+case 2: goto e;
+case 3: goto h;
+case 4: goto a;
+case 5: goto q;
+}
+exit(1);
+
+function menuitem() {
+global $a;
+$a=$a+1;
+print $a;
+print " ";
+return;
+}
+
+function sign() {
+global $c;
+if ((1==$c) or (2==$c)) print "+";
+if ((3==$c) or (4==$c)) print "-";
+if (5==$c) print "*";
+if (6==$c) print "/";
+return;
+}
+
+function pow10() {
+global $r, $i, $a;
+$r=1;
+for ($i=1; $i<=$a; ++$i) {
+$r=$r*10;
+} # $i
+return;
+}
+
+function delay() {
+global $d;
+$d=$d*1000;
+usleep($d);
+return;
+}
+
+function cls() {
+global $t;
+for ($t=1; $t<=40; ++$t) {
+print "\n";
+} # $t
+print "aritm\n";
+return;
+}
+
+function help() {
+global $d;
+cls();
+print "help\n";
+print "you can mix problems anyway you like.\n";
+print "more help on <http://aritm.orbin.se/>.\n";
+$d=5000;
+delay();
+return;
+}
+
+function about() {
+global $d;
+cls();
+print "about\n";
+print "aritm 0.3 (c) 1992-2019 by\n";
+print "mikael o. bonnier, lund, sweden.\n";
+print "absolutely no warranty.\n";
+print "foss, see license gplv3+.\n";
+$d=5000;
+delay();
+return;
+}
+
+function sound () {
+fprintf(STDOUT, "%s", "\x07");
+usleep(100000);
+return;
+}
+?>
