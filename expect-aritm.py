e70f2bdc99fc42dd977162f0579cd0e465a9a10e
Mikael Bonnier <mikael.bonnier@gmail.com>
2022-01-24 07:45:22 +0100

    Improved tests

--- expect-aritm.py
+++ expect-aritm.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import pexpect
 import math
-#s='php aritm.php'
-s='pcbasic aritm-gw.bas -n -i=stdio'
-#s='../zce/zce -r0 -xc aritm-zx81.bas'
-print(s)
-child=pexpect.spawn(s)
-s=': ' # PHP/GW-BASIC
-#s='> ' # ZX81
+#c='php aritm.php'
+#c='pcbasic aritm-gw.bas -n -i=stdio'
+c='../zce/zce -r0 -xc aritm-zx81.bas'
+#s=': ' # PHP/GW-BASIC
+s='> ' # ZX81
+#b,e=-6,-1 # PHP/GW-BASIC
+b,e=-9,-4 # ZX81
+print(c)
+child=pexpect.spawn(c)
+n=5*100+90-1
 child.expect(s)
 print('L1')
 child.sendline('1')
@@ -31,10 +34,10 @@ print('L3')
 child.sendline('0')
 child.expect('[=>] ')
 print('L4')
-for i in range(589): # 1-5: 99, 6 (div): 89
-    s=child.before[-6:-1] # PHP/GW-BASIC, 4 (sub 2): -6, 6 (div): -5
-    #s=child.before[-7:-4] # ZX81
-    s = s.replace(b'f',b' ')
+for i in range(n):
+    s=child.before[b:e]
+    s = s.replace(b'f',b' ') # PHP/GW-BASIC
+    s = s.replace(b'F',b' ') # ZX81
     print(s)
     r=int(math.floor(eval(s)))
     print('%i: result %g'%(i,r))
