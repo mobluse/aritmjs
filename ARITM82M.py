2ebca9e527c44bbfddbdc42c2cdba394733e11df
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2025-09-23 16:48:19 +0200

    Update ARITM82M.py

--- ARITM82M.py
+++ ARITM82M.py
@@ -1,6 +1,5 @@
 # Aritm Mini v0.3.4 for TI-82 Advanced Edition Python V5.6.5 is FOSS.
 # (c) 1992-2025 by Mikael O. Bonnier, Lund, Sweden.
-# E-mail: <mikael.bonnier@gmail.com>.
 # Absolutely no warranty.
 # License GPLv3+, see
 # <http://www.gnu.org/licenses/gpl.html>.
@@ -11,6 +10,8 @@
 
 from time import sleep, monotonic
 from random import random
+from gc import collect#, mem_free
+from atco import co
 
 l = 0
 m = 10
@@ -30,7 +31,7 @@ def rnd():
   return random()
 
 def cls():
-  for i in range(10):
+  for i in range(8):
     print()
   print("Aritm")
 
@@ -38,8 +39,8 @@ def sign(c):
   global sb
   if c in [1, 2]:
     sb += '+'
-  elif c in [3, 4]:
-    sb += '-'
+#  elif c in [3, 4]:
+#    sb += '-'
   elif c == 5:
     sb += '*'
   elif c == 6:
@@ -52,8 +53,7 @@ def sign(c):
 #  print("anyway you like.")
 #  print("More help on")
 #  print("<http://aritm.orbin.se/>.")
-#  d = 5000
-#  delay(d)
+#  delay(5000)
 
 #def about():
 #  cls()
@@ -62,8 +62,7 @@ def sign(c):
 #  print("Mikael O. Bonnier, Lund,")
 #  print("Absolutely no warranty.")
 #  print("FOSS, see license GPLv3+.")
-#  d = 5000
-#  delay(d)
+#  delay(5000)
 
 def menuitem():
   global a
@@ -157,7 +156,6 @@ def generate():
         t = i * j + int(j * rnd())
         aa[u] = 60000000 + t * 10000 + j * 100 + 1
         u += 1
-
   u -= 1
   l = u
 
@@ -173,12 +171,10 @@ def shuffle():
 
 def practice():
   global k, u, l, aa, sb
-
   while l > 0:
     while k <= u and l > 0:
       cls()
-      print("%d problems left. -1 Esc"%l)
-
+      print("%s%d problems left.%s %s-1 Esc%s"%(co(4,7),l,co(0,7),co(11,7),co(0,7)))
       t = aa[k]
       x = t
       y = 10000000
@@ -187,7 +183,6 @@ def practice():
       sb = ''
       if c == 6:
           sb += 'Integer part of '
-
       x = t
       y = 10000
       dv = x//y
@@ -196,9 +191,7 @@ def practice():
       md = x%y
       i = md
       sb += str(i)
-
       sign(c)
-
       x = t
       y = 100
       dv = x//y
@@ -208,22 +201,19 @@ def practice():
       j = md
       sb += str(j)
       sb += ' = '
-
       ans = input(sb)
       try:
         a = float(ans)
       except:
-        a = -2
-
+        a = -9
       if a == -1 or ans in ['.1', ',1', '01']:
         return
-      if a == -2:
+      if a == -9:
         continue
-
       if c in [1, 2]:
         r = i + j
-      elif c in [3, 4]:
-        r = i - j
+#      elif c in [3, 4]:
+#        r = i - j
       elif c == 5:
         r = i * j
       elif c == 6:
@@ -231,10 +221,9 @@ def practice():
         y = j
         dv = x//y
         r = dv
-
       if r != a:
         sb = ''
-        sb += 'Wrong. '
+        sb += '%sWrong. %s'%(co(1,7),co(0,7))
         sb += str(i)
         sign(c)
         sb += str(j)
@@ -242,19 +231,16 @@ def practice():
         sb += str(r)
         sb += '.'
         print(sb)
-
         x = t
         y = 100
         md = x%y
         if md < 99:
           l += 1
           aa[k] = t + 1
-
         k += 1
-        d = 1300
-        delay(d)
+        delay(1300)
       else:
-        print("Right!")
+        print("%sRight!%s"%(co(2,7),co(0,7)))
         x = t
         y = 100
         md = x%y
@@ -263,13 +249,10 @@ def practice():
           aa[k] = t - 1
         k += 1
         if l > 0:
-          d = 600
-          delay(d)
-
+          delay(600)
     if l <= 0:
       print("Good!!! Well done!")
-      d = 5000
-      delay(d)
+      delay(5000)
       return
     else:
       print("Checking...")
@@ -299,12 +282,10 @@ def nr():
   print(end='%d%c'%(a,('*' if md != 0 else ' ')))
 
 def setupMenu():
-  global m, n, a, k, _seed
-
+  global m, n, a, k
   while True:
     cls()
     n = 0
-
     for a in range(1, 7):
       r = 10**a
       x = m
@@ -315,44 +296,32 @@ def setupMenu():
       md = x%y
       if md != 0:
         n += 90 if a == 6 else 100
-
     a = 0
     nr()
     print("Addition 1")
-
     nr()
     print("N/A")
-
     nr()
     print("N/A")
-
     nr()
     print("N/A")
-
     nr()
     print("Multiplication")
-
     nr()
     print("Division|-1 Esc")
-
     print('0 OK and go %d'%n)
-
     while True:
       ans = input('Toggle 1-6, or 0 or -1: ')
       try:
         a = int(ans)
       except:
         a = -2
-
       if a == -1 or ans in ['.1', ',1', '01']:
         return
-
       if ans == "" or a < -1 or a > 6 or (a == 0 and n == 0):
         continue
-
       if a == 0:
         if n > 0:
-          _seed = int(monotonic()*1000000) & 0xFFFFFFFF
           generate()
           shuffle()
           k = 1
@@ -361,7 +330,6 @@ def setupMenu():
         else:
           continue
       break
-
     r = 10**a
     x = m
     y = r
@@ -369,7 +337,6 @@ def setupMenu():
     x = dv
     y = 10
     md = x%y
-
     if md == 0:
       m += r
     else:
@@ -377,14 +344,14 @@ def setupMenu():
 
 def menu():
   global l, m, a
-
+#  print(mem_free())
+  collect()
+#  print(mem_free())
   cls()
   a = 0
-
   if l > 0:
     menuitem()
     print("Continue")
-
   menuitem()
   print("Setup and go")
   menuitem()
@@ -393,23 +360,21 @@ def menu():
   print("N/A")
   menuitem()
   print("Exit")
-
   s = 0
-
   while s < 1 or s > a:
     try:
       s = int(input('Choose 1-%d: '%a))
     except:
       s = 0
-
   oi = s if l > 0 else s + 1
-
   if oi == 1:
     practice()
   elif oi == 2:
     setupMenu()
-  elif oi== 4:
-    pass
+  #elif oi== 3:
+  #  pass
+  #elif oi== 4:
+  #  pass
   elif oi == 5:
     quit()
 
