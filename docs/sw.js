e9ca7c32d0a8fb150463b51895a113c414e19e6f
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2026-02-07 22:25:06 +0100

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'aritm-v1';
+const ASSETS = [
+  './',
+  './index.html'
+];
+
+// Installera Service Worker och cacha filer
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Aktivera och rensa gamla cachar
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// "Network-first" strategi för att tillåta uppdateringar, men falla tillbaka på cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
